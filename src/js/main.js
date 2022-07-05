de1ec6abcd7ab0eeb016201c43eafe1ab949fd74
Ирина <ielkinairinka@gmail.com>
2022-07-05 22:38:21 +0300

    +

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
+// import * as webpFunctions from "./modules/webp.js";
+// import * as modulesFunctions from "./modules/modals.js";
+// webpFunctions.isWebp();
+// modulesFunctions.modals();
 $(function () {
-
-})
+  $('.header__btn').on('click', function(){
+    $('.rightside-menu').revoveClass('rightside-menu--close');
+  });
+  $('.rightside-menu__close').on('click', function(){
+    $('.rightside-menu').addClass('rightside-menu--close');
+  });
+});
